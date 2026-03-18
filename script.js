6b1c15eea175be1d6b2375047d5b102efb6c6cf1
ThomazVitorino <thomazvitorino0@gmail.com>
2026-03-17 21:07:13 -0300

    aula 3

---
+++ script.js
@@ -0,0 +1,56 @@
+
+
+const { useImperativeHandle } = require("react")
+
+console.log("Mensagem normal")
+
+console.info("Informação")
+
+console.warn("Aviso")
+
+console.error("Erro")
+
+
+
+console.table([
+    {
+        id: 1, tarefa: "EstudarJS"
+    },
+    {
+         id: 2, tarefa: "Praticar código"
+   }
+])
+
+console.group("Grupo de logs")
+console.log('log 1')
+console.log('log 2')
+console.log('log 3')
+console.groupEnd()
+
+console.time("Timer")
+
+let casa = ""
+if (casa == "casa") {
+    // console.log('casa')
+}
+
+console.timeEnd("Timer")
+Comentário de uma linha
+ 
+hfhifhiufhehifuhf
+fiaufaçiwufhulai
+huifaçwfuieafhçe
+iufbaeçfuieafuiç
+sfhsfhahuilfaulh
+
+
+var antigo = "Forma antiga de declarar variáveis"
+
+
+let userName = "Maria"
+
+const API_URL = "url"
+ 
+ let numero = 42
+console.log("nome = "+ numero
+ + " tipo:"+ typeof(numero))
